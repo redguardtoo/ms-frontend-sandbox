7965e9938d4b8e1d203c6efe32d43b94daeaf27b
Chen Bin <chenbin.sh@gmail.com>
2018-08-27 11:35:47 +1000

    test setup

--- frontend/test/.setup.js
+++ frontend/test/.setup.js
@@ -1,6 +1,9 @@
 require('babel-register')();
+var configure = require('enzyme').configure;
+var Adapter = require('enzyme-adapter-react-15.4');
 var jsdom = require('jsdom').jsdom;
 const hook = require('css-modules-require-hook');
+configure({ adapter: new Adapter() });
 hook({
   // create clean and unified className which make it easiert to write test cases
   // because every test case is totally independent, it's impossible to have className
